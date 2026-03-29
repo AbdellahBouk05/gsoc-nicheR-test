9c10804ec6b8e199a60638bc637151e06213c634
AbdellahBouk05 <bdallhbwk@gmail.com>
2026-03-29 22:17:19 +0200

    test.R

---
+++ test.R
@@ -0,0 +1,15 @@
+install.packages("terra")
+install.packages("geodata")
+
+library(terra)
+library(geodata)
+
+clim <- worldclim_global(var = "bio", res = 10, path = tempdir())
+bio1 <- clim[[1]]
+south_america <- ext(-90, -30, -60, 15)
+bio1_crop <- crop(bio1, south_america)
+plot(bio1_crop, main = "BIO1 - Température moyenne annuelle (Amérique du Sud)")
+
+png("bio1_south_america.png")
+plot(bio1_crop)
+dev.off()
